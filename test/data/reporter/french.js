8021eeb7e187d2afe49b28acad24e3402a3d5d54
Sébastien Règne <regseb@gmail.com>
2016-05-28 19:56:33 +0200

    Réusiner le code de normalisation.

---
+++ test/data/reporter/french.js
@@ -0,0 +1,50 @@
+"use strict";
+
+/**
+ * Ne pas écrire les résultats ; retourner seulement la sévérité la plus élévée.
+ *
+ * @param {Promise.<Object>} promise Une promesse retournant la liste des
+ *                                   notifications regroupées par fichier.
+ * @param {Object}           writer  Le flux où écrire les résultats.
+ * @return {Promise.<number>} Une promesse retournant la sévérité la plus élévée
+ *                            des résultats.
+ */
+const reporter = function (promise, writer) {
+    return promise.then(function (results) {
+        let severity = null;
+        for (const file in results) {
+            // Si le fichier n'a pas été vérifié (car il ne rentrait pas dans
+            // les critères des checkers).
+            if (null === results[file]) {
+                continue;
+            }
+
+            for (const notice of results[file]) {
+                // Déterminer la sévérité la plus élévée des résultats.
+                if (null === severity || severity > notice.severity) {
+                    severity = notice.severity;
+                }
+
+                writer.write("Le linter " + notice.linter + " a trouvé");
+                if (null === notice.rule) {
+                    writer.write(" un problème");
+                } else {
+                    writer.write(" que la règle " + notice.rule + " n'est pas" +
+                                 " respectée");
+                }
+                if (0 === notice.locations.length) {
+                    writer.write(" dans le fichier ");
+                } else {
+                    writer.write(" à la ligne " +
+                                 notice.locations[0].line.toString() + " du" +
+                                 " fichier ");
+                }
+                writer.write(file + " : " + notice.message + "\n");
+            }
+        }
+
+        return severity;
+    });
+}; // reporter()
+
+module.exports = reporter;
---
+++ test/lib/normalize.js
@@ -0,0 +1,164 @@
+"use strict";
+
+const assert    = require("assert");
+const path      = require("path");
+const normalize = require("../../lib/normalize.js");
+const SEVERITY  = require("../../lib/severity.js");
+const console   = require("../../lib/reporter/console.js");
+const csv       = require("../../lib/reporter/csv.js");
+
+describe("lib/normalize.js", function () {
+    it("", function () {
+        const rotten = { "checkers": [{ "linters": { "eslint": {} } }] };
+        const standard = normalize(rotten,
+                                   path.join(__dirname, "../data/.metalint"));
+        assert.deepStrictEqual(standard, {
+            "patterns": ["**"],
+            "hidden":   false,
+            "level":    SEVERITY.INFO,
+            "reporter": console,
+            "verbose":  0,
+            "output":   process.stdout,
+            "checkers": [
+                {
+                    "patterns": ["**"],
+                    "hidden": false,
+                    "level": SEVERITY.INFO,
+                    "linters": {
+                        "eslint": {}
+                    }
+                }
+            ]
+        });
+    });
+
+    it("", function () {
+        const rotten = {
+            "patterns": "**.js",
+            "hidden":   true,
+            "level":    "ERROR",
+            "reporter": "csv",
+            "verbose":  2,
+            "output":   null,
+            "checkers": [
+                {
+                    "hidden":   false,
+                    "level":    "INFO",
+                    "linters":  "markdownlint"
+                }, {
+                    "patterns": ["!**.min.js", "**"],
+                    "linters": {
+                        "eslint": [
+                            "eslint.json",
+                            { "rules":  { "curly": 1, "no-var": 2 } }
+                        ]
+                    }
+                }, {
+                    "linters": ["htmllint", "htmlhint"]
+                }, {
+                    "linters": { "csslint": "csslintrc" }
+                }
+            ]
+        };
+        const standard = normalize(rotten,
+                                   path.join(__dirname, "../data/.metalint"));
+        assert.deepEqual(standard, {
+            "patterns": ["**.js"],
+            "hidden":   true,
+            "level":    SEVERITY.ERROR,
+            "reporter": csv,
+            "verbose":  2,
+            "output":   process.stdout,
+            "checkers": [
+                {
+                    "patterns": ["**"],
+                    "hidden":   false,
+                    "level":    SEVERITY.ERROR,
+                    "linters": {
+                        "markdownlint": { "MD035": { "style": "---" } }
+                    }
+                }, {
+                    "patterns": ["!**.min.js", "**"],
+                    "hidden":   true,
+                    "level":    SEVERITY.ERROR,
+                    "linters": {
+                        "eslint": {
+                            "rules": { "no-empty": 2, "curly": 1, "no-var": 2 }
+                        }
+                    }
+                }, {
+                    "patterns": ["**"],
+                    "hidden":   true,
+                    "level":    SEVERITY.ERROR,
+                    "linters": {
+                        "htmlhint": { "tagname-lowercase": true },
+                        "htmllint": { "doctype-html5": true }
+                    }
+                }, {
+                    "patterns": ["**"],
+                    "hidden":   true,
+                    "level":    SEVERITY.ERROR,
+                    "linters":  { "csslint": { "empty-rules": true } }
+                }
+            ]
+        });
+    });
+
+    it("", function () {
+        const runfail = function (rotten) {
+            try {
+                normalize(rotten, null);
+                assert.fail();
+            } catch (_) {
+                // Ne rien faire.
+            }
+        }; // runfail()
+
+        const rotten = {
+            "checkers": [{ "linters": { "jsonlint": [1] } }]
+        };
+        runfail(rotten);
+
+        rotten.checkers[0].linters.jsonlint[0] = null;
+        runfail(rotten);
+
+        rotten.checkers[0].linters.jsonlint = 1;
+        runfail(rotten);
+
+        rotten.checkers[0].linters = 1;
+        runfail(rotten);
+
+        rotten.checkers[0].linters = null;
+        runfail(rotten);
+
+        delete rotten.checkers[0].linters;
+        runfail(rotten);
+
+        rotten.checkers.length = 0;
+        runfail(rotten);
+
+        rotten.checkers = 1;
+        runfail(rotten);
+
+        rotten.output = 1;
+        runfail(rotten);
+
+        rotten.verbose = "Blablabla";
+        runfail(rotten);
+
+        rotten.reporter = 1;
+        runfail(rotten);
+
+        rotten.level = "APOCALYPSE";
+        runfail(rotten);
+
+        rotten.level = 1;
+        runfail(rotten);
+
+        rotten.hidden = 1;
+        runfail(rotten);
+
+        rotten.patterns = 1;
+        runfail(rotten);
+    });
+});
